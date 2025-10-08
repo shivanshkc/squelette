6ad0e281c799af44f29b2b822e1f10a96cd345af
Shivansh Kuchchal <shivanshbox@gmail.com>
2025-10-08 17:40:13 +0530

    feat(config): simplify config management, no viper
    BREAKING CHANGE: yaml config is no longer supported

--- cmd/squelette/main.go
+++ cmd/squelette/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,15 +16,24 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
-	// Initialize basic dependencies.
-	conf := config.Load()
+	// Allow the user to maintain multiple config files and switch between them conveniently.
+	configPath := flag.String("config", "config/config.json", "config file path")
+	flag.Parse()
+
+	// Prime dependency.
+	conf, err := config.Load(*configPath)
+	if err != nil {
+		panic("failed to load config: " + err.Error())
+	}
+
+	// Set up the global logger. It can be used through slog.
 	logger.Init(os.Stdout, conf.Logger.Level, conf.Logger.Pretty)
 
 	// Initialize the HTTP server.
 	server := &http.Server{}
 
 	// Start the http server. The server will shut down when the context expires.
-	if err := server.Start(ctx, conf.HTTPServer.Addr); err != nil {
+	if err := server.Start(ctx, conf.HttpServer.Addr); err != nil {
 		panic("error in server.Start call: " + err.Error())
 	}
 }
